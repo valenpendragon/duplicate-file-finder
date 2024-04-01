58ca8f917b0f39c70f31be4a46ca08a2fc19068c
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-01 10:14:14 -0400

    Add versioning information and package docstrings to all current __init__.py files.

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,1 +1,5 @@
+"""Top-level package for functions required for duplicate-file-finder."""
+
+__version__ = '0.2.0'
+
 from .file_functions import file_hash
---
+++ rptree/__init__.py
@@ -0,0 +1,3 @@
+"""Top-level package for RP Tree app."""
+
+__version__ = '0.1.0'
