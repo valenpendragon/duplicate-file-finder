16ff0062e5d72a50a094ff570f1a404fe7501f45
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-01 10:28:53 -0400

    Add basic docstring to rptree.py. Add imports and constants to generate tree appearances.

---
+++ rptree/cli/rptree.py
@@ -0,0 +1,13 @@
+"""
+This module provides the RP tree main module.
+"""
+
+import os
+import pathlib
+
+# Constants needed to generate the tree appearance in the CLI.
+PIPE = "│"
+ELBOW = "└──"
+TEE = "├──"
+PIPE_PREFIX = "│   "
+SPACE_PREFIX = "    "
