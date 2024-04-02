ac7430b524c89a3cce6309b60ac6b12adb7b0a4a
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-02 15:24:27 -0400

    Update version after previous bug fix to classes DirectoryObject and FileObject.

--- classes/__init__.py
+++ classes/__init__.py
@@ -2,6 +2,6 @@
 Top-level module for directory tree objects derived from treelib.
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .directory_structures import DirectoryObject, FileObject
