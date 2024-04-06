c1c3333738eb4d6ba643a45a55a017803b50fe77
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-06 13:49:40 -0400

    Create class DirectoryTree and low level class _TreeGenerator. Add generator method to DirectoryTree. Update version for RP Tree package.

--- rptree/rptree.py
+++ rptree/rptree.py
@@ -8,3 +8,25 @@ ELBOW = "└──"
 TEE = "├──"
 PIPE_PREFIX = "│   "
 SPACE_PREFIX = "    "
+
+
+class DirectoryTree:
+    def __init__(self, root_dir):
+        """
+        This method requires the path to the root directory where the
+        DirectoryTree will begin. This is a required parameeter, but it
+        may be a relative path instead of an absolute path.
+        :param root_dir: filepath
+        """
+        self._generator = _TreeGenerator(root_dir)
+
+    def generate(self):
+        """This method prints out the directory tree to STDIO"""
+        tree = self._generator.build_tree()
+        for entry in tree:
+            print(entry)
+
+
+
+class _TreeGenerator:
+    pass
