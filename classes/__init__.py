5d3ed6ad851a946cbc9b5643489f2bf62f43f42c
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-06 12:34:51 -0400

    Scrap rebuilding instead of using the treelib library as is.

--- classes/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Top-level module for directory tree objects derived from treelib.
-"""
-
-__version__ = '0.4.0'
-
-from .directory_structures import (DirectoryObject, FileObject,
-                                   DirectoryTree)
