ba738e34194096e03c63e00c222a1cdc1f8b6364
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-07 08:04:58 -0400

    Correct typo and error in DirectoryTree.__init__() docstring.  Add __init__, build_tree and _tree_head methods to _TreeGenerator. Add skeleton method _tree_body to _TreeGenerator.

--- rptree/rptree.py
+++ rptree/rptree.py
@@ -13,10 +13,10 @@ SPACE_PREFIX = "    "
 class DirectoryTree:
     def __init__(self, root_dir):
         """
-        This method requires the path to the root directory where the
-        DirectoryTree will begin. This is a required parameeter, but it
+        This method requires the filepath to the root directory where the
+        DirectoryTree will begin. This is a required parameter, but it
         may be a relative path instead of an absolute path.
-        :param root_dir: filepath
+        :param root_dir: str of a filepath
         """
         self._generator = _TreeGenerator(root_dir)
 
@@ -27,6 +27,32 @@ class DirectoryTree:
             print(entry)
 
 
-
 class _TreeGenerator:
-    pass
+    def __init__(self, root_dir):
+        """
+        This method requires the filepath to the root directory where the
+        _TreeGenerator will begin. This is a required parameter, but it
+        may be a relative path instead of an absolute path. This class adds
+        an important attribute to DirectoryTree.
+        :param root_dir: str of a filepath
+        """
+        self._root_dir = pathlib.Path(root_dir)
+        self._tree = []
+
+    def build_tree(self):
+        """
+        This method builds the tree structure for a DirectoryTree, storing it
+        in the internal attributes of this object as well as returning the
+        structure to make it available to other classes.
+        :return: _tree, a nested list of nodes forming the directory tree.
+        """
+        self._tree_head()
+        self._tree_body(self._root_dir)
+        return self._tree
+
+    def _tree_head(self):
+        self._tree.append(f"{self._root_dir}{os.sep}")
+        self._tree.append(PIPE)
+
+    def _tree_body(self, root_dir):
+        pass
