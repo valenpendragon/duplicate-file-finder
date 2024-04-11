95505f4362618add117f03f31467ee6fdfea1be0
Jeffrey Scott <valenpendragon@gmail.com>
2024-04-11 14:55:48 -0400

    Add imports to cli.py in RP Tree package. Add parse_cmd_line_arguments and main() functions to cli.

--- rptree/__init__.py
+++ rptree/__init__.py
@@ -1,3 +1,3 @@
 """Top level package for RP Tree package."""
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
---
+++ rptree/cli.py
@@ -0,0 +1,37 @@
+import argparse
+import pathlib
+import sys
+
+from . import __version__
+from .rptree import DirectoryTree
+
+
+def parse_cmd_line_arguments():
+    parser = argparse.ArgumentParser(
+        prog="tree",
+        description="RP Tree directory tree generator",
+        epilog="RP Tree Help"
+    )
+    parser.version = f"RP Tree v{__version__}"
+    parser.add_argument("-v", "--version",
+                        action="version")
+    parser.add_argument(
+        "root_dir",
+        metavar="ROOT_DIR",
+        nargs="?",
+        default='.',
+        help="Create a full directory tree at ROOT_DIR."
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_cmd_line_arguments()
+    root_dir = pathlib.Path(args.root_dir)
+    if not root_dir.is_dir():
+        error_msg = (f"Argument, {args.root_dir}, does not "
+                     f"exist or is not a directory.")
+        print(error_msg)
+        sys.exit()
+    tree = DirectoryTree(root_dir)
+    tree.generate()
